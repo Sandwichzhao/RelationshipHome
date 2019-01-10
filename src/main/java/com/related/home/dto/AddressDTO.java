c28181b794881a0d339a9ac672e1c9cdc82f5c78
Sandwich <yuzhoufengg2@126.com>
2019-01-10 23:04:49 +0800

    update

--- src/main/java/com/related/home/dto/AddressDTO.java
+++ src/main/java/com/related/home/dto/AddressDTO.java
@@ -1,7 +1,31 @@
 package com.related.home.dto;
 
 public class AddressDTO {
-    private String province;
-    private String city;
+    private String provinceCode;
+    private String cityCode;
     private String detailInfo;
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getDetailInfo() {
+        return detailInfo;
+    }
+
+    public void setDetailInfo(String detailInfo) {
+        this.detailInfo = detailInfo;
+    }
 }
--- src/main/java/com/related/home/service/UserService.java
+++ src/main/java/com/related/home/service/UserService.java
@@ -1,9 +1,17 @@
 package com.related.home.service;
 
+import com.related.home.dto.AddressDTO;
+import com.related.home.dto.UserInfoDTO;
 import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
 
 @Service
 public class UserService {
+
+    @Transactional
+    public void registerUserInfo(UserInfoDTO userInfoDTO) {
+        AddressDTO addressDTO = userInfoDTO.getAddress();
+    }
 
 }
 
